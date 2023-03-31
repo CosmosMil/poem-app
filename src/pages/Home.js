da71344587e9d5bbc0b62fdb1b3ab4f9e6f2327f
CosmosMil <107584409+CosmosMil@users.noreply.github.com>
2023-03-31 16:58:23 +0200

    added fire-store functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,68 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar';
+import React, { useEffect, useState } from 'react';
+import { AuthContext } from "../contexts/AuthContext";
+import { useContext } from "react";
+import {
+  addDoc,
+  collection,
+} from "firebase/firestore";
+import { db } from '../firebase';
 
 function Home() {
     const [poem, setPoem] = useState([]);
+     const { user } = useContext(AuthContext);
+
 
     useEffect(() => {
         const fetchPoem = async () => {
             const response = await fetch('https://poetrydb.org/random');
             const result = await response.json();
             setPoem(result[0]);
-            console.log(result)
+            console.log('test result' , result);
+            
 
         }
         fetchPoem()
     },   [])
 
 
+        const handleClick = () => {
+        window.location.assign('/collection');
+
+    }
+  
+    async function addPoemToCollection() {
+   
+        const userEmail = user.email;
+        const userId = user.uid;
+
+    const poemData = {
+        title: poem.title,
+        author: poem.author,
+        poem: poem.lines,
+      userId: userId,
+      userEmail: userEmail,
+    
+    };
+    try {
+      const docRef = await addDoc(collection(db, "fav.poem"), poemData);
+      console.log("poem added - test");
+    } catch (e) {
+      console.error('Error adding poem');
+    }
+  }
+
+
+
     return (
 
         <>
       
-        <div className='grid grid-col-1 justify-center w-full'>
-            {/* <div className='w-screen'>
-             <Navbar /></div> */}
-            
-
-              <h1 className='text-3xl text-center font-bold p-10 text-lime-400'>random poem</h1>
+            <div className='grid grid-col-1 justify-center w-full'>
+                <h1 className='text-3xl text-center font-bold p-3 text-lime-400'>random poem</h1>
           
-            <div className='text-center p-10'>
-                <div className='bg-lime-400 inline-block p-10 rounded'>
+            <div className='text-center p-3'>
+                    <div className='bg-lime-400 inline-block p-5 rounded'>
+                        <div className='flex justify-end m-6'><button onClick={addPoemToCollection} className='  bg-gray-500 text-lime-400 rounded h-8 w-28'>save poem</button></div>
               <h2 className='text-xl text-gray-500'>{poem.title} by {poem.author}</h2><br/>
               
               <p className='text-gray-500'>{poem.lines && 
--- src/pages/MyCollection.js
+++ src/pages/MyCollection.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useContext } from 'react'
+import { useState } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
 
-function MyCollection() {
+function MyCollection({poem}) {
+  const { user } = useContext(AuthContext);
+  const [favPoem, setFavPoem] = useState([]);
+  
+  
   return (
-    <div>MyCollection</div>
+    <>
+      <h1 className='text-3xl text-center font-bold p-10 text-lime-400'>my collection</h1>
+       {favPoem.map((poem) => (
+        <div key={poem.id}>
+          <h2>{poem.title} by {poem.author}</h2>
+          {poem.lines.map((line, index) => (
+            <p key={index}>{line}</p>
+          ))}
+        </div>
+      ))}
+  
+    </>
   )
 }
 
