224646652597aef0ece612c7ab3d05eb4cf975ab
CosmosMil <107584409+CosmosMil@users.noreply.github.com>
2023-04-04 15:01:19 +0200

    save button and functionality added

--- src/pages/MyCollection.js
+++ src/pages/MyCollection.js
@@ -1,10 +1,7 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { AuthContext } from '../contexts/AuthContext';
-import { db } from '../firebase';
-import { doc, collection, getDocs } from 'firebase/firestore';
-
-
-
+import React, { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { db } from "../firebase";
+import { doc, collection, getDocs } from "firebase/firestore";
 
 function MyCollection() {
   const { user } = useContext(AuthContext);
@@ -13,18 +10,15 @@ function MyCollection() {
 
   // console.log('authUser in MyCollection:', user);
 
- 
-
   useEffect(() => {
     const fetchFavPoems = async () => {
       if (user) {
         const userRef = doc(db, `users/${user.uid}`);
-        const favPoemsRef = collection(userRef, 'favPoems');
+        const favPoemsRef = collection(userRef, "favPoems");
 
         // console.log('authUser.uid:', user.uid);
         // console.log('userRef:', userRef);
         // console.log('favPoemsRef:', favPoemsRef);
-      
 
         const querySnapshot = await getDocs(favPoemsRef);
         const poems = [];
@@ -35,38 +29,50 @@ function MyCollection() {
         setFavPoem(poems);
       }
     };
-  
+
     fetchFavPoems();
   }, [user]);
-  
 
-  
- 
-  
   return (
     <>
-      <h1 className='text-3xl text-center font-bold p-10 text-lime-400'>my collection</h1>
+      <h1 className="text-3xl text-center font-bold p-10 text-lime-400">
+        my collection
+      </h1>
       {favPoem.map((poem) => (
-        <div className='text-center p-10' key={poem.title}>
-          <div className='bg-lime-400 inline-block p-3 rounded w-2/3'>
-            <div className='flex justify-end m-6'>
-              <button onClick={() => setShowPoem(poem.title)} className='bg-gray-500 text-lime-400 rounded h-8 w-28'>open</button>
+        <div className="text-center p-10" key={poem.title}>
+          <div className="bg-lime-400 inline-block p-3 rounded w-2/3">
+            <div className="flex justify-end m-6">
+              <button
+                onClick={() => setShowPoem(poem.title)}
+                className="bg-gray-500 text-lime-400 rounded h-8 w-28"
+              >
+                open
+              </button>
             </div>
-            
-            <div className='p-5 border-2 border-gray-700 border-dotted rounded'>
-              <h2 className='text-xl text-gray-500 font-semibold'>{poem.title}</h2>
-              <h3 className='text-xl text-gray-500'>by {poem.author}</h3><br />
+
+            <div className="p-5 border-2 border-gray-700 border-dotted rounded">
+              <h2 className="text-xl text-gray-500 font-semibold">
+                {poem.title}
+              </h2>
+              <h3 className="text-xl text-gray-500">by {poem.author}</h3>
+              <br />
               {showPoem === poem.title && (
-                < div className='text-center p-10'>
-                <button onClick={() => setShowPoem(false)} className='bg-gray-500 text-lime-400 rounded h-8 w-28'>close</button>
-                <p className='text-gray-500'>
-                  {poem.lines &&
-                    poem.lines.map((line, index) => (
-                      <React.Fragment key={index}>
-                        {line} <br />
-                      </React.Fragment>
-                    ))}
-                </p> </div>
+                <div className="text-center p-10">
+                  <button
+                    onClick={() => setShowPoem(false)}
+                    className="bg-gray-500 text-lime-400 rounded h-8 w-28"
+                  >
+                    close
+                  </button>
+                  <p className="text-gray-500">
+                    {poem.lines &&
+                      poem.lines.map((line, index) => (
+                        <React.Fragment key={index}>
+                          {line} <br />
+                        </React.Fragment>
+                      ))}
+                  </p>
+                </div>
               )}
             </div>
           </div>
@@ -76,4 +82,4 @@ function MyCollection() {
   );
 }
 
-export default MyCollection
+export default MyCollection;
